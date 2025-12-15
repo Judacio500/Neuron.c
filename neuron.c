0f564a757c87837060ef373442976b26da9b7a2a
Judacio500 <darkiman18@gmail.com>
2025-12-14 19:41:12 -0600

    Added: Back propagation logic

--- neuron.c
+++ neuron.c
@@ -1,6 +1,6 @@
 #include"neuron.h"
 
-MODEL *createModel()
+MODEL *createModel(float learningRate)
 {
     MODEL *newM = (MODEL*)malloc(sizeof(MODEL));
 
@@ -8,6 +8,10 @@ MODEL *createModel()
         return NULL;
 
     newM->layers = createWrap(); // Queue wrapper for the node list
+    newM->currentLayer = NULL;
+    newM->learningRate = learningRate;
+    newM->inputs = NULL;
+    newM->targets = NULL;
 
     return newM; 
 }
@@ -21,7 +25,9 @@ LAYER *createLayer(int nNeurons, Actv activation)
 
     newL->nNeurons = nNeurons;
     newL->activation = activation;
-    newL->perceptrons = NULL;
+    newL->dActivation = dSigmoid;   // Hardcoded the activation derivative
+                                    // this function will be assigned automatically when there is more functions
+    newL->perceptrons = NULL;   
     newL->currentLayer = NULL;
 
     return newL;
@@ -184,12 +190,114 @@ int stepForward(MODEL *network)
     return 0; // "Keep dancing"
 }
 
+int stepBackward(MODEL *network) 
+{
+    if (!network->currentLayer) 
+        return 1;
+
+    LAYER *currL = (LAYER*)network->currentLayer->data;
+    LIST *nodeList = currL->perceptrons;
+    
+    if (network->currentLayer->next == NULL) 
+    { 
+        int t_idx = 0;
+        while(nodeList)
+        {
+            NODE *n = (NODE*)nodeList->data;
+            PERCEPTRON *p = (PERCEPTRON*)n->data;
+
+
+            // this is treated as a window, so targets is currently pointing
+            // at the start of a certain "row" in the flattened array
+            // each neuron represents what that neuron should be for each target column
+            /*
+                For example:
+                Let's say we have 3 classes (this would be for a multi class problem)
+                0 1 2 3 4 5 6 7 8
+                1 0 0 0 0 1 0 1 0
+                |   | |   | |   |
+                
+                The lines above define the dimensions of the array for each itteraation
+                when we are sitting at position 0, we are defining that the neural network should (not must because crashes and segmentations are free)
+                the same amount of output neurons as the amount of classes we have in the data set 
+            */    
+        
+            float output = p->output;      // What we guessed
+            float target = network->targets[t_idx];      // What it should be 
+
+            
+            // Gradient = Error * Derivative
+            // Error = (target - output)
+            // Derivative = dSigmoid(output)
+            p->gradient = (target - output) * currL->dActivation(output); // output was already the sigmoid 
+                                                                                  // value, hence we are just calling
+                                                                                  // the derivative with the output of the neuron
+            
+            p->bias += network->learningRate * p->gradient;
+
+            nodeList = nodeList->next;
+            t_idx++;
+        }
+    } 
+    
+    else 
+    {
+        while(nodeList) 
+        {
+            NODE *n = (NODE*)nodeList->data;
+            PERCEPTRON *p = (PERCEPTRON*)n->data;
+            float output = p->output;
+            
+            float acumGradient = 0;
+
+            EDGE *connection = n->firstEdge;
+            while(connection) 
+            {
+                NODE *nextN = connection->destinationNode;  // extract the nodes from the next layer
+                                                            // one at a time
+                PERCEPTRON *nextP = (PERCEPTRON*)nextN->data;
+
+                // Chain Rule
+                // My fault = (Next Node's Fault) * (Strength of Connection)
+                acumGradient += nextP->gradient * connection->weight; 
+                
+                // NewWeight = OldWeight + (LearningRate * Gradient * MyInput)
+                // Basically 
+                // The value i sent you (MyInput)
+                // How much i want it to affect the gradient (LearningRate)
+                // In which direction should we step to get it right (Gradient)
+                
+                // This is beautiful because we just depend from the node updated gradient
+                // effectively propagating the error
+                float delta = network->learningRate * nextP->gradient * output;
+                connection->weight += delta;
+
+                connection = connection->next; // Next node i affected
+            }
+            
+            // Now we know the total blame (acumGradient).
+            // How much did i (the perceptron) affected that the result was not the expected
+            // Multiply by my own derivative to save it for the Previous Layer.
+            p->gradient = acumGradient * currL->dActivation(output);
+            p->bias += network->learningRate * p->gradient;
+
+            nodeList = nodeList->next;
+        }
+    }
+
+    network->currentLayer = network->currentLayer->prev;
+
+    if (network->currentLayer == NULL) 
+        return 1; 
+    return 0;
+}
+
 float sigmoid(float x) 
 {
     return 1 / (1 + expf(-x));
 }
 
-float d_sigmoid(float output) 
+float dSigmoid(float output) 
 {
     return output * (1 - output);
 }
